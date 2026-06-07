c8de1fe676ff2da59402806f3f50a9edee0962ba
Elad Levavi <eladlevavi@gmail.com>
2026-06-07 21:50:16 +0300

    implement submissions api functions

--- Controllers/SubmissionsController.cs
+++ Controllers/SubmissionsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Rubberduck.Api;
+using Rubberduck.Api.Models;
 
 namespace Rubberduck.Controllers;
 
@@ -19,5 +20,59 @@ public class SubmissionsController : ControllerBase
     {
         var submissions = _dbContext.Submissions.ToList();
         return Ok(submissions);
+    }
+
+    [HttpPost]
+    public IActionResult CreateSubmission(Submission submission)
+    {
+        _dbContext.Submissions.Add(submission);
+        _dbContext.SaveChanges();
+        return CreatedAtAction(nameof(GetSubmissions), new { id = submission.Id }, submission);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetSubmission(Guid id)
+    {
+        var submission = _dbContext.Submissions.Find(id);
+
+        if (submission == null)
+        {
+            return NotFound();
+        }
+        return Ok(submission);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateSubmissions(Guid id, Submission updatedSubmission)
+    {
+        var submission = _dbContext.Submissions.Find(id);
+
+        if (submission == null)
+        {
+            return NotFound();
+        }
+        
+        submission.Title = updatedSubmission.Title;
+        submission.Language = updatedSubmission.Language;
+        submission.Code = updatedSubmission.Code;
+        _dbContext.SaveChanges();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteSubmission(Guid id)
+    {
+        var submission = _dbContext.Submissions.Find(id);
+
+        if (submission == null)
+        {
+            return NotFound();
+        }
+        
+        _dbContext.Submissions.Remove(submission);
+        _dbContext.SaveChanges();
+
+        return NoContent();
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Rubberduck.Api;
+using Scalar.AspNetCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +20,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
